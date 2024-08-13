953de41aba37b8ba64c48b4b80025a6e1fedd878
kuznecnbs <KuznechikTV@Yandex.kz>
2024-08-13 16:37:00 +0500

    Update main.swift

--- theRouletters/theRouletters/main.swift
+++ theRouletters/theRouletters/main.swift
@@ -1,14 +1,15 @@
 import Foundation
 
-var player1: String?
-var player2: String?
+var player1: String = "Игрок 1"
+var player2: String = "Игрок 2"
 var turn = true //true == player1, false == player2
 var life1 = 3, life2 = 3, item_number: Int
-var choise1 = -1, choise2 = -1 //0 взять оружие, 1 стрельба во врага, 2 стрельба в себя
+var choise = "-1"//0 взять оружие, 1 стрельба во врага, 2 стрельба в себя
 var inv1: [String] = ["-", "-", "-", "-"]
 var inv2: [String] = ["-", "-", "-", "-"]
-var shot = 0
 var action = 7
+var currentCamora = 0
+var holdGun = false
 
 var armor1 = false, armor2 = false, double_damage = false
 var baraban: [Bool] = [false, false, false, false, false, false]
@@ -19,11 +20,27 @@ func clearConsole(){
     print("\u{001b}[H")
 }
 
-func items(){
-    
+func addItems(){
+    var count = 0
+    for _ in 1...2{
+        for j in 1...4{
+            if inv1[j] == "-"{
+                inv1[j] = item_list[Int.random(in: 0...2)]
+                break
+            }
+        }
+    }
+    for _ in 1...2{
+        for j in 1...4{
+            if inv2[j] == "-"{
+                inv2[j] = item_list[Int.random(in: 0...2)]
+                break
+            }
+        }
+    }
 }
 
-func new_roulete(){
+func reloadBaraban(){
     for i in 0...5{
         baraban[i] = false
     }
@@ -37,35 +54,94 @@ func new_roulete(){
             }
         }
     }
+    currentCamora = 0
     print("Сегодня патронов у нас будет:", bullets)
-    sleep(2)
+    sleep(1)
+}
+
+func rotate_baraban(){
+    currentCamora += 1
+}
+
+
+func printInterface(){
+    if turn == true{
+        clearConsole()
+        print("Очередь", player1, "нажать на спуск\n")
+        print(player2, "\tHP =", life2)
+        if armor2 == true{
+            print("\tARM")
+        }
+        print("")
+        for i in 0...3{
+            print(i+1, ":", inv2[i], ";", terminator: "")
+        }
+        if choise == "-1"{
+            print("\n\nНажми 0 чтобы взять пушку\n\n")
+        }
+        else if choise == "0" && holdGun == true{
+            print("\n\nНажми 1 чтобы стрельнуть в опонента\n", "\nНажми 2 чтобы стрельнуть в себя")
+        }
+        else if choise == "1" && holdGun == true{
+            
+        }
+        else if choise == "2" && holdGun == true{
+            
+        }
+        print(player1, "HP =", life1, terminator: "")
+        if armor1 == true{
+            print("\tARM", terminator: "")
+        }
+        if double_damage == true{
+            print("\tDMG+")
+        }
+        print("")
+        for i in 0...3{
+            print(i+1, ":", inv1[i], ";", terminator: "")
+        }
+        print("")
+    }
+    else{
+        
+    }
 }
 
 func game(){
     inv1[0] = item_list[Int.random(in: 0...2)];inv1[1] = "-";inv1[2] = "-";inv1[3] = "-";
     inv2[0] = item_list[Int.random(in: 0...2)];inv2[1] = "-";inv2[2] = "-";inv2[3] = "-";
     life1 = 3; life2 = 3
-    new_roulete()
+    reloadBaraban()
     turn = true
     while true{
         action = 7
+        choise = "-1"
         if life1 == 0{
             clearConsole()
-            print("Поздравляю ", player2 ?? "Игрок 2", " с его победой")
+            print("Поздравляю ", player2, " с его победой")
             sleep(3)
             return
         }
         else if life2 == 0{
             clearConsole()
-            print("Поздравляю ", player1 ?? "Игрок 1", " с его победой")
+            print("Поздравляю ", player1, " с его победой")
             sleep(3)
             return
         }
-        if shot == 6{
+        if currentCamora >= 6{
             print("Кхм, вы сделали 6 выстрелов и ещё живы?\nВремя нового раунда")
-            shot = 0
-            items()
-            new_roulete()
+            addItems()
+            reloadBaraban()
+        }
+        if turn == true{
+            while choise != "0"{
+                printInterface()
+                choise = readLine() ?? "-1"
+                
+            }
+            holdGun = true
+        }
+        else{
+            
         }
     }
 }
@@ -73,9 +149,15 @@ func game(){
 func players(){
     clearConsole()
     print("Введите имя для игрока 1")
-    player1 = readLine()
+    player1 = readLine() ?? "Игрок 1"
+    if player1 == ""{
+        player1 = "Игрок 1"
+    }
     print("Введите имя для игрока 2")
-    player2 = readLine()
+    player2 = readLine() ?? "Игрок 2"
+    if player2 == ""{
+        player2 = "Игрок 2"
+    }
     game()
 }
 
